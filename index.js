bbfca9668a39c5b240be08fbbad7c4318efc688e
Gaukhar Abdizhapbarova <gauhar.abdizhapbarova@gmail.com>
2022-08-14 21:50:34 +0600

    Add createBookCard function Add functions to modal buttons, elements

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
+const modal = document.querySelector('.modal');
+const newBookBtn = document.querySelector('.library-content__button--add');
+const closeModalElem = document.querySelector('.close-element');
+const bookTitle = document.querySelector('.modal-content__input--title');
+const bookAuthor = document.querySelector('.modal-content__input--author');
+const bookPages = document.querySelector('.modal-content__input--pages');
+const readBook = document.querySelector('.modal-content__input--checkbox');
+const submitBookBtn = document.querySelector('.modal-content__button');
 
+submitBookBtn.onclick = function() {
+    addBookToLibrary();
+    displayBook();
+}
+
+newBookBtn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+closeModalElem.onclick = function() {
+    modal.style.display = 'none';
+    bookTitle.value = '';
+    bookAuthor.value ='';
+    bookPages.value = '';
+    readBook.checked = false;
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = 'none';
+        bookTitle.value = '';
+        bookAuthor.value ='';
+        bookPages.value = '';
+        readBook.checked = false;
+    }
+}
 
 let myLibrary = [];
 
@@ -7,27 +41,56 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${this.title} ${this.author}, ${this.pages} pages, ${this.read}`
-    };
 };
 
 function addBookToLibrary() {
-    const title = prompt('Title?','');
-    const author = prompt('Author?','');
-    const pages = prompt('Pages?','');
-    const read = prompt('Have you read it?','');
-
-    const book = new Book(title, author, pages, read);
+    const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readBook.checked);
     myLibrary.push(book);
 };
 
 function displayBook() {
 myLibrary.forEach((book) => {
-    let bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    bookCard.textContent += book.info();
-    document.querySelector('.library-content__cards').appendChild(bookCard);
+    let bookCard = createBookCard(book);
+
 });
 };
 
+const createBookCard = (book) => {
+    let bookCard = document.createElement('div');
+    let title = document.createElement('div');
+    let author = document.createElement('div');
+    let pages = document.createElement('div');
+    let readBook = document.createElement('button');
+    let deleteBook = document.createElement('button');
+
+    bookCard.classList.add('book-card');
+    title.classList.add('book-card__item');
+    author.classList.add('book-card__item');
+    pages.classList.add('book-card__item');
+    readBook.classList.add('book-card__item');
+    deleteBook.classList.add('book-card__item');
+
+    title.textContent = `"${book.title}"`;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    deleteBook.textContent = 'Delete';
+    if(book.read == true) {
+        readBook.textContent = 'Read';
+    } else {
+        readBook.textContent = 'Not read';
+    }
+
+
+    document.querySelector('.library-content__cards').appendChild(bookCard);
+    document.querySelector('.book-card').appendChild(title);
+    document.querySelector('.book-card').appendChild(author);
+    document.querySelector('.book-card').appendChild(pages);
+    document.querySelector('.book-card').appendChild(readBook);
+    document.querySelector('.book-card').appendChild(deleteBook);
+}
+
+function updateBookCard() {
+
+}
+
+createBookCard();
